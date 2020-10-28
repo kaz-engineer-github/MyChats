8c303e52d911e0f2d0c57f8efd1f9fa2c52e7ce4
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-10-28 15:20:22 +0900

    add cell

---
+++ MyChats/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  MyChats
+//
+//  Created by 吉本和史 on 2020/10/28.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
